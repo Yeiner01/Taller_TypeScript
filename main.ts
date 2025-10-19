e885e4a0f12903d3d244bb3084e670b29573dded
Yeiner Ibarguen <y.ibarguenm@uniandes.edu.co>
2025-10-19 14:43:06 -0500

    ass

---
+++ main.ts
@@ -0,0 +1,25 @@
+import { series } from "./data.ts";
+
+const tableBody = document.querySelector("#series-table tbody")!;
+const averageSpan = document.querySelector("#average")! as HTMLSpanElement;
+
+function loadSeries(): void {
+  let totalTemporadas = 0;
+
+  series.forEach(serie => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${serie.id}</td>
+      <td>${serie.nombre}</td>
+      <td>${serie.canal}</td>
+      <td>${serie.temporadas}</td>
+    `;
+    tableBody.appendChild(row);
+    totalTemporadas += serie.temporadas;
+  });
+
+  const avg = totalTemporadas / series.length;
+  averageSpan.textContent = avg.toFixed(1);
+}
+
+loadSeries();
